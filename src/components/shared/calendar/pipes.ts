d05c836292044ecd957184a414549a6a897c68a0
Anton Breslavsky <breslavsky.anton@gmail.com>
2019-02-05 16:09:58 +0300

    use AOT for prod

--- src/components/shared/calendar/pipes.ts
+++
@@ -1,9 +0,0 @@
-import {Pipe, PipeTransform} from '@angular/core';
-import {Moment} from 'moment';
-
-@Pipe({name: 'startOfMonth'})
-export class StartOfMonth implements PipeTransform {
-  transform(date: Moment): Moment {
-    return date.startOf('month');
-  }
-}
