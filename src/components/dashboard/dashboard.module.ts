04ebe1f842928ea76b42f1df1e9fbdaf192fce40
juliaselezneva <juli.89@mail.ru>
2019-04-08 19:06:21 +0300

    #27 integrate encapsulated junte-ui

--- src/components/dashboard/dashboard.module.ts
+++ src/components/dashboard/dashboard.module.ts
@@ -2,8 +2,6 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {DashboardRoutingModule} from './dashboard-routing.module';
 import {DashboardComponent} from './dashboard.component';
-import {HeaderComponent} from './header/header.component';
-import {FooterComponent} from './footer/footer.component';
 import {AuthorizationGuard} from '../../guards/authorization.guard';
 import {JunteUiModule} from 'junte-ui';
 import {ArrayPipesModule} from '../../pipes/array-pipes.module';
@@ -11,8 +9,6 @@ import {ArrayPipesModule} from '../../pipes/array-pipes.module';
 @NgModule({
   declarations: [
     DashboardComponent,
-    HeaderComponent,
-    FooterComponent
   ],
   imports: [
     CommonModule,
