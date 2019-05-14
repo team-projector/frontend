526d20b5bf8ac134a30654311dfb036b4737748d
Vladimir Smirnov <ex700380@gmail.com>
2019-05-14 15:52:29 +0300

    #60 list of salary

--- src/components/developer/developer.module.ts
+++ src/components/developer/developer.module.ts
@@ -1,8 +1,14 @@
-import {NgModule} from '@angular/core';
-import {DeveloperRoutingModule} from './developer-routing.module';
+import { NgModule } from '@angular/core';
+import { DeveloperRoutingModule } from './developer-routing.module';
+import { DeveloperComponent } from './developer.component';
+import { JunteUiModule } from 'junte-ui';
 
 @NgModule({
+  declarations: [
+    DeveloperComponent
+  ],
   imports: [
+    JunteUiModule,
     DeveloperRoutingModule
   ]
 })
