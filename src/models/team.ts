8a5c06c7fa6d054731fa3c0cf4135400b20f2f2b
Roman Gromov <hanaevroman@gmail.com>
2019-05-17 11:09:40 +0000

    Resolve "Select a team"

--- src/models/team.ts
+++ src/models/team.ts
@@ -1,9 +1,8 @@
-import {ArraySerializer, ModelSerializer, Field, Type, Model} from 'serialize-ts';
-import {MockClass, MockField, MockFieldNested} from '../decorators/mock';
-import {UserCard} from './user';
-import {Paging} from './paging';
-import {IssueCard} from './issue';
-import {PrimitiveSerializer} from 'serialize-ts/dist';
+import { ArraySerializer, Field, Model, ModelSerializer, Type } from 'serialize-ts';
+import { MockClass, MockField, MockFieldNested } from '../decorators/mock';
+import { UserCard } from './user';
+import { Paging } from './paging';
+import { PrimitiveSerializer } from 'serialize-ts/dist';
 
 export enum TeamMemberRole {
   developer = 'developer',
@@ -22,6 +21,14 @@ export class TeamCard {
   @MockField('{{team}}')
   title: string;
 
+  @Field()
+  @MockField('{{int 3 10}}')
+  membersCount: number;
+
+  @Field()
+  @Type(new ArraySerializer(new ModelSerializer(UserCard)))
+  @MockFieldNested('[{{#repeat 5 15}} {{> user_card }} {{/repeat}}]')
+  members: UserCard[];
 }
 
 @Model()
@@ -29,14 +36,13 @@ export class TeamCard {
 export class PagingTeams implements Paging<TeamCard> {
 
   @Field()
-  @MockFieldNested('{{int 50 1000}}')
+  @MockFieldNested('{{int 3 10}}')
   count: number;
 
   @Field()
   @Type(new ArraySerializer(new ModelSerializer(TeamCard)))
-  @MockFieldNested('[{{#repeat 10 20}} {{> team_card}} {{/repeat}}]')
-  results: IssueCard[];
-
+  @MockFieldNested('[{{#repeat 3 10}} {{> team_card}} {{/repeat}}]')
+  results: TeamCard[];
 }
 
 @Model()
@@ -59,12 +65,12 @@ export class TeamMemberCard {
 export class PagingTeamMembers implements Paging<TeamMemberCard> {
 
   @Field()
-  @MockFieldNested('{{int 50 1000}}')
+  @MockFieldNested('{{int 2 7}}')
   count: number;
 
   @Field()
   @Type(new ArraySerializer(new ModelSerializer(TeamMemberCard)))
-  @MockFieldNested('[{{#repeat 10 20}} {{> team_member_card}} {{/repeat}}]')
+  @MockFieldNested('[{{#repeat 2 7}} {{> team_member_card}} {{/repeat}}]')
   results: TeamMemberCard[];
 
 }
---
+++ src/resolvers/team-members.ts
@@ -0,0 +1,18 @@
+import { Inject, Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from '@angular/router';
+import { Observable } from 'rxjs';
+import { ITeamsService, teams_service } from '../services/teams/interface';
+import { PagingTeamMembers } from '../models/team';
+
+@Injectable()
+export class TeamMembersResolver implements Resolve<Observable<PagingTeamMembers>> {
+
+  constructor(@Inject(teams_service) private teamsService: ITeamsService) {
+  }
+
+  resolve(route: ActivatedRouteSnapshot,
+          state: RouterStateSnapshot): Observable<PagingTeamMembers> {
+    const team = +route.params['team'];
+    return !!team ? this.teamsService.teamMembers(team) : null;
+  }
+}
