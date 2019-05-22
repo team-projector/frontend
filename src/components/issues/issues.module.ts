68ff6411b9b9569def61d7669c91d2f6f620b67c
juliaselezneva <juli.89@mail.ru>
2019-05-22 15:23:23 +0300

    #72 integrate new version of table component

--- src/components/issues/issues.module.ts
+++ src/components/issues/issues.module.ts
@@ -3,7 +3,6 @@ import {CommonModule} from '@angular/common';
 import {ReactiveFormsModule} from '@angular/forms';
 import {MetricsServiceProvider} from '../../services/metrics/provider';
 import {IssuesServiceProvider} from '../../services/issues/provider';
-import {DeveloperDashboardComponent} from '../developer/dashboard/developer-dashboard.component';
 import {DatePipesModule} from '../../pipes/date-pipes.module';
 import {ArrayPipesModule} from '../../pipes/array-pipes.module';
 import {TimeExpensesServiceProvider} from '../../services/time-expenses/provider';
