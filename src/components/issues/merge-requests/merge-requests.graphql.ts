a1ad1447e15d89bf69c5750de3308b6ee9d31b38
Vladimir Smirnov <ex700380@gmail.com>
2019-08-30 14:46:38 +0300

    #180 badges in merge request

--- src/components/issues/merge-requests/merge-requests.graphql.ts
+++ src/components/issues/merge-requests/merge-requests.graphql.ts
@@ -1,6 +1,6 @@
-import {Injectable} from '@angular/core';
+import { Injectable } from '@angular/core';
 import gql from 'graphql-tag';
-import {Mutation, Query} from 'apollo-angular';
+import { Query } from 'apollo-angular';
 
 @Injectable({
   providedIn: 'root'
@@ -8,52 +8,67 @@ import {Mutation, Query} from 'apollo-angular';
 export class MergeRequestsGQL extends Query<{ mergeRequests }> {
   document = gql`
     query MergeRequests($team: ID, $user: ID, $project: ID, $state: String, $orderBy: String, $offset: Int, $first: Int) {
-  mergeRequests: allMergeRequests(team: $team, user: $user, project: $project, state: $state, orderBy: $orderBy, offset: $offset, first: $first) {
-    count
-    edges {
-      node {
-        title
-        id
-        labels {
-          count
-          edges {
-            node {
-              title
-              color
+      mergeRequests: allMergeRequests(team: $team, user: $user, project: $project, state: $state, orderBy: $orderBy, offset: $offset, first: $first) {
+        count
+        edges {
+          node {
+            title
+            id
+            labels {
+              count
+              edges {
+                node {
+                  title
+                  color
+                }
+              }
             }
-          }
-        }
-        project {
-          fullTitle
-        }
-        state
-        createdAt
-        timeEstimate
-        totalTimeSpent
-        timeEstimate
-        glUrl
-        user {
-          id
-          name
-          glAvatar
-        }
-        participants {
-          count
-          edges {
-            node {
+            project {
+              fullTitle
+            }
+            state
+            createdAt
+            timeEstimate
+            totalTimeSpent
+            timeEstimate
+            glUrl
+            user {
+              id
               name
               glAvatar
             }
+            participants {
+              count
+              edges {
+                node {
+                  name
+                  glAvatar
+                }
+              }
+            }
+            metrics {
+              remains
+              efficiency
+              payroll
+              paid
+            }
           }
         }
-        metrics {
-          remains
-          efficiency
-          payroll
-          paid
-        }
       }
-    }
-  }
-}`;
+    }`;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MergeRequestSummaryGQL extends Query<{ summary }> {
+  document = gql`
+    query MergeRequestsSummaryType($team: ID, $user: ID, $project: ID) {
+      summary: mergeRequestsSummary(team: $team, user: $user, project: $project) {
+        count
+        openedCount
+        closedCount
+        mergedCount
+      }
+    }`;
 }
