12218532a33d6a01a67a47f9094c107cc20aca9e
Joe <joefamwork@gmail.com>
2019-12-16 12:03:45 +0300

    269

---
+++ src/components/leader/teams/team/breaks/team-breaks.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { UI } from 'junte-ui';
+
+
+@Component({
+  selector: 'app-team-breaks',
+  templateUrl: './team-breaks.component.html',
+  styleUrls: ['./team-breaks.component.scss']
+})
+export class TeamBreaksComponent {
+  ui = UI;
+}
