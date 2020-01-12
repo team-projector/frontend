58aed1ee0ee29fcda4be172226fe9d4ec03da8ba
Anton Breslavsky <breslavsky.anton@gmail.com>
2020-01-12 18:02:21 +0300

    signin page message

--- src/assets/themes/themes.js
+++ src/assets/themes/themes.js
@@ -1,5 +1,5 @@
 function themes(theme, callback) {
-  var themes = {"dark":"581b32155c","light":"c513b42743"};
+  var themes = {"dark":"d48399429c","light":"c513b42743"};
 
   var clear = function () {
     var exist = document.head.querySelector('style[theme="true"]');
