3db1c9dfa15058f06687bcb37e19b3e60e8099f0
Anton Breslavsky <breslavsky.anton@gmail.com>
2020-01-22 13:35:21 +0300

    adjust top icons

--- src/enums/local-ui.ts
+++ src/enums/local-ui.ts
@@ -1,41 +1,9 @@
-export enum SvgLocalIcons {
-  developer = 'developer',
-  teamLeader = 'team-leader',
-  projectManager = 'project-manager',
-  customer = 'customer',
-  shareholder = 'shareholder'
-}
-
-export const SvgIcons = {...SvgLocalIcons};
-export type SvgIcons = SvgLocalIcons;
-
-export enum TypeIcon {
-  svg = 'svg'
-}
-
-class ShortIcons {
-  static icons = {
-    developer: SvgLocalIcons.developer + ':svg',
-    teamLeader: SvgLocalIcons.teamLeader + ':svg',
-    projectManager: SvgLocalIcons.projectManager + ':svg',
-    customer: SvgLocalIcons.customer + ':svg',
-    shareholder: SvgLocalIcons.shareholder + ':svg'
-  };
-}
-
 export class LocalUI {
   static icons = {
-    menu: {
-      svg: {
-        local: {
-          developer: SvgLocalIcons.developer,
-          teamLeader: SvgLocalIcons.teamLeader,
-          projectManager: SvgLocalIcons.projectManager,
-          customer: SvgLocalIcons.customer,
-          shareholder: SvgLocalIcons.shareholder
-        }
-      },
-      ...ShortIcons.icons
-    }
+    developer: 'developer:svg:local',
+    teamLeader: 'team-leader:svg:local',
+    projectManager: 'project-manager:svg:local',
+    customer: 'customer:svg:local',
+    shareholder: 'shareholder:svg:local'
   };
 }
