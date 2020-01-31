8d362f692401f2de724401be4f1d2dde62eb5896
Anton Breslavsky <breslavsky.anton@gmail.com>
2020-01-31 13:45:12 +0300

    headless for e2e

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -11,7 +11,7 @@ exports.config = {
   capabilities: {
     'browserName': 'chrome',
     chromeOptions: {
-      args: ['--no-sandbox', '--disable-gpu', '--window-size=1600x1000'],
+      args: ['--headless', '--no-sandbox', '--disable-gpu', '--window-size=1600x1000'],
       // args: ['--no-sandbox', '--disable-gpu', '--window-size=1600x1000']
     }
   },
