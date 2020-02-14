0fb3b46998ce9ac908231d1fa3bc463ec58e80ed
Anton Breslavsky <breslavsky.anton@gmail.com>
2020-02-14 14:33:27 +0300

    work break icon

--- src/components/developer/developer.component.ts
+++ src/components/developer/developer.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { UI } from 'junte-ui';
+import { LocalUI } from '../../enums/local-ui';
 
 @Component({
   selector: 'app-developer',
@@ -8,5 +9,8 @@ import { UI } from 'junte-ui';
 })
 
 export class DeveloperComponent {
+
   ui = UI;
+  localUi = LocalUI;
+
 }
