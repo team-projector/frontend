31fcfa02ae45f80bfe409c7208ebbb87a32b0caf
Anton Breslavsky <breslavsky.anton@gmail.com>
2020-02-14 18:32:13 +0300

    master review due date

--- src/components/due-date/due-date.pipe.ts
+++ src/components/due-date/due-date.pipe.ts
@@ -1,16 +1,5 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import {
-  addDays,
-  differenceInDays,
-  endOfWeek,
-  format,
-  isSameWeek,
-  isThisWeek,
-  isThisYear,
-  isToday,
-  isTomorrow,
-  isYesterday, startOfDay
-} from 'date-fns';
+import { differenceInDays, format, isThisWeek, isThisYear, isToday, isTomorrow, isYesterday, startOfDay } from 'date-fns';
 
 @Pipe({
   name: 'dueDate'
@@ -18,24 +7,23 @@ import {
 export class DueDatePipe implements PipeTransform {
   transform(dueDate: Date): string {
 
+    const comingDay = (d) => isTomorrow(d)
+      ? $localize`:@@label.tomorrow:Tomorrow`
+      : isToday(d) ? $localize`:@@label.today:Today` :
+        (isYesterday(d)
+          ? $localize`:@@label.yesterday:Yesterday`
+          : format(d, 'd MMMM yyyy'));
+
     const today = startOfDay(new Date());
-
-    const endNextWeek = endOfWeek(addDays(today, 7), {weekStartsOn: 1});
     const difference = differenceInDays(dueDate, today);
-    const comingDay = (d) => isTomorrow(d) ? $localize`:@@label.tomorrow:Tomorrow` : isToday(d) ? $localize`:@@label.today:Today` :
-      isYesterday(d) ? $localize`:@@label.yesterday:Yesterday` : format(d, 'd MMMM yyyy');
-
-    let date: string = null;
     if (difference < 2) {
-      date = comingDay(dueDate);
+      return comingDay(dueDate);
     } else if (isThisWeek(dueDate, {weekStartsOn: 1})) {
-      date = format(dueDate, 'EEEE');
+      return format(dueDate, 'EEEE');
     } else if (isThisYear(dueDate)) {
-      date = format(dueDate, 'd MMMM');
-    } else {
-      date = format(dueDate, 'd MMMM yyyy');
+      return format(dueDate, 'd MMMM');
     }
 
-    return date;
+    return format(dueDate, 'd MMMM yyyy');
   }
 }
