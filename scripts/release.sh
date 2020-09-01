d4eedb3a9ec7f076f993fb25e69d3c12df296d2a
Stanislav Lapshin <staslapshin88@gmail.com>
2020-09-01 14:50:55 +0300

    [#] ci refactor

---
+++ scripts/release.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+
+set -o errexit
+set -o nounset
+
+git_branch=$(git symbolic-ref --short -q HEAD)
+echo ${git_branch}
+if [ "${git_branch}" != "prod" ]
+then
+  echo "Error! Release should be on \"prod\" branch."
+  exit 1
+fi
+
+version=$(cat VERSION)
+git tag -a ${version} -m "v${version}"
+git push
+git push --tags
