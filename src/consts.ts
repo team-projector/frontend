5f409c156d4ae0dee14ec146e6f87c0e5f464747
Anton Breslavsky <breslavsky.anton@gmail.com>
2020-10-09 11:33:31 +0300

    fix backend config

--- src/consts.ts
+++ src/consts.ts
@@ -9,7 +9,11 @@ import { DateFnsConfigurationService } from 'ngx-date-fns';
 import { detectLanguage } from 'src/utils/lang';
 import { Language } from './enums/language';
 
-declare const backend: { config: { firstWeekDay: 0 | 1 } };
+const backend: { config: { firstWeekDay: 0 | 1 } } = window['backend'] || {
+  config: {
+    firstWeekDay: 0
+  }
+};
 
 export const PLATFORM_DELAY = 100;
 export const UI_DELAY = 250;
@@ -26,7 +30,7 @@ enum Currencies {
 }
 
 const CURRENCY_CODE: Currencies = Currencies.rur;
-const FIRST_DAY_OF_WEEK: 0 | 1 | 2 | 3 | 4 | 5 | 6 = backend?.config?.firstWeekDay || 0;
+const FIRST_DAY_OF_WEEK: 0 | 1 | 2 | 3 | 4 | 5 | 6 = backend.config.firstWeekDay || 0;
 
 const fnsConfig = new DateFnsConfigurationService();
 
